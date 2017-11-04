1c59b53256be671de40db97643860d665d23feb9
akshay <akshaysonavane0@gmail.com>
2017-11-04 16:35:45 +0530

    baground modified
    bg tag

--- VILD.js
+++ VILD.js
@@ -1,30 +1,4 @@
-(function(f){
-	if(typeof exports==="object"&&typeof module!=="undefined")
-		{
-			module.exports=f()
-		}else if(typeof define==="function"&&define.amd)
-		{
-			define([],f)
-		}
-		else
-			{
-				var g;
-		if(typeof window!=="undefined")
-		{
-			g=window
-		}else if(typeof global!=="undefined")
-		{
-			g=global
-		}
-		else if(typeof self!=="undefined")
-			{
-				g=self
-			}
-			else
-				{
-					g=this}g.AFRAME = f()
-				}
-			})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.AFRAME = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
   'use strict';
   // For more information about browser field, check out the browser field at https://github.com/substack/browserify-handbook#browser-field.
 
@@ -64167,7 +64141,7 @@ calls being made, such as tooling or effect injectors.
                                                                 var registerGeometry = _dereq_('../core/geometry').registerGeometry;
                                                                 var THREE = _dereq_('../lib/three');
 
-                                                                registerGeometry('cube', {
+                                                                registerGeometry('box', {
                                                                   schema: {
                                                                     depth: {default: 1, min: 0},
                                                                     height: {default: 1, min: 0},
@@ -66204,8 +66178,10 @@ calls being made, such as tooling or effect injectors.
                                                                 module.exports.transformKeysToCamelCase = transformKeysToCamelCase;
 
                                                               },{"style-attr":12}],124:[function(_dereq_,module,exports){
-                                                               
-
+                                                                /**
+ * @author dmarcos / https://github.com/dmarcos
+ * @author mrdoob / http://mrdoob.com
+ */
 
                                                                 THREE.VRControls = function ( object, onError ) {
 
@@ -66379,6 +66355,16 @@ calls being made, such as tooling or effect injectors.
                                                                 };
 
                                                               },{}],125:[function(_dereq_,module,exports){
+                                                                /**
+ * @author dmarcos / https://github.com/dmarcos
+ * @author mrdoob / http://mrdoob.com
+ *
+ * WebVR Spec: http://mozvr.github.io/webvr-spec/webvr.html
+ *
+ * Firefox: http://mozvr.com/downloads/
+ * Chromium: https://webvr.info/get-chrome
+ *
+ */
 
                                                                 THREE.VREffect = function ( renderer, onError ) {
 
@@ -67545,6 +67531,20 @@ calls being made, such as tooling or effect injectors.
                                                                 }
 
                                                               },{}],128:[function(_dereq_,module,exports){
+                                                                /*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
                                                                 var Util = {};
 
@@ -67594,7 +67594,19 @@ calls being made, such as tooling or effect injectors.
 
                                                               },{}],129:[function(_dereq_,module,exports){
                                                                 /*
-
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
                                                                 var Util = _dereq_('./util.js');
 
@@ -67658,3 +67670,4 @@ calls being made, such as tooling or effect injectors.
 
                                                               },{"./util.js":128}]},{},[101])(101)
   });
+  //# sourceMappingURL=aframe.js.map
